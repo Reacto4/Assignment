e11a4bba66b4498562aa376020fc62cbd2704625
Reacto4 <151157236+Reacto4@users.noreply.github.com>
2024-03-17 21:00:12 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
 //Define script variables
 
 let guessCount = 0;
-let maxGuess = 3;
-let userName = prompt("Please, enter you name!")
+let maxGuess = 5;
+
+let arr = [];
+
+
+const userName = document.querySelector("#sbmtName");
+const userNa = document.querySelector("#sub");
+const result = document.getElementsByClassName("item");
+const endBtn = document.querySelector("#end");
+const list = document.querySelector(".ui.list");
+const scoreName = document.querySelector(".content")
+
+
 
 const form = document.querySelector("form");
 const message = document.querySelector("#message");
@@ -15,8 +26,14 @@ const submitBtn = document.querySelector("[type='submit']");
 let num = Math.round(Math.random() * 10);
 
 
-const header = document.querySelector('h1')
-header.textContent = 'Hello ' + userName + ' welcome to the guessing game!'
+userNa.addEventListener('click', (evt) => {
+  evt.preventDefault()
+  const header = document.querySelector('h1')
+  header.textContent = `Hello, ` + userName.value.toUpperCase() + ` , welcome to the guessing game!`
+  console.log(userName.value);
+  
+ })
+
 
 const guessInput = form.elements.guess;
 console.log(guessInput.value) // 
@@ -52,21 +69,43 @@ againBtn.addEventListener('click', (evt) => {
          correct.style.display ="";
          message.textContent = "Start guessing...Again!";
          submitBtn.disabled = false;
-    document.getElementById("message").style.background = "revert";  
+        document.getElementById("message").style.background = "revert";
+        document.getElementById("score").innerHTML = 0;
+        
      
         })
+
+        endBtn.addEventListener('click', (evt) => {
+
+          console.log('End button working');
+              myFunction();
+            // Create Strawberry
+            arr.splice(0,arr.length);
+            
+            
+      
+         })
   
 
   function check(){
 
    if(guessInput.value == num){
       message.textContent = 'You guessed it right!'
-      correct.style.display = "flex";
-      bestScore.textContent = guessCount;
+      correct.style.display = "block";
       submitBtn.disabled = true;
        
-        guessInput.value = "";
+      guessInput.value = "";
       guess.focus();
+      storeArray();
+      bestScore.textContent = guessCount; 
+      let min;
+      if (arr.length > 0) {
+          min = Math.min(...arr);
+      } else {
+          min = 0; // Set a default value if the array is empty
+      }
+      bestScore.textContent = min;
+      console.log(min);
       
     }
       
@@ -90,8 +129,31 @@ againBtn.addEventListener('click', (evt) => {
    }
 
   }
+
+  function storeArray(){
+    arr.push(guessCount);
+    console.log(arr);
+  }
+
   
- 
 
 
- 
+  function myFunction() {
+
+    let sum = 0;
+    arr.forEach( num => {
+    sum += num});
+
+    let table = document.getElementById("scoreTable");
+    let row = table.insertRow(1);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    cell1.innerHTML = userName.value;
+    cell2.innerHTML = sum;
+  
+}
+
+
+
+  
+    
